386c05ebd26c97c43eeea6d7e3fcf74015ac40a8
Daniel Ochoa <dannytenaglias@gmail.com>
2017-04-23 21:14:58 -0500

    use react-router-dom. build quick sample of how it works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Main from './Main';
+import About from './About';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
+
+  state = {
+    personId: null,
+  }
+
+  componentDidMount() {
+    this.setState({ personId: 'me' });
+  }
+
   render() {
+    const { personId } = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            {/* routes */}
+            <Route path='/' exact={true} render={() => (
+              <Main>
+                <p className="App-intro">
+                  To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>Link to <Link to={`/about/${personId}`}>someplace</Link>.</p>
+              </Main>
+            )}/>
+
+            <Route path='/about/:personId' component={About} />
+
+          </div>
+        </Router>
       </div>
     );
   }
